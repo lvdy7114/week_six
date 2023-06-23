eada06aeb3b1942a6d6947df1bb312ecba2840a8
Linda Vadysirisack <linda.vady@gmail.com>
2023-06-23 15:24:56 -0500

    wk 6 work complete

--- day_four/ajax_fetch_axios_lab/carousel.js
+++ day_four/ajax_fetch_axios_lab/carousel.js
@@ -1,4 +1,4 @@
-import * as bootstrap from "bootstrap";
+/*import * as bootstrap from "bootstrap";*/
 import { favourite } from "./index.js";
 
 export function createCarouselItem(imgSrc, imgAlt, imgId) {
@@ -38,9 +38,11 @@ export function start() {
     "#carouselExampleControls"
   );
   if (window.matchMedia("(min-width: 768px)").matches) {
-    const carousel = new bootstrap.Carousel(multipleCardCarousel, {
+   /* const carousel = new bootstrap.Carousel(multipleCardCarousel, {
       interval: false
-    });
+    });*/
+    $('#carouselExampleControls').carousel();
+
     const carouselWidth = $(".carousel-inner")[0].scrollWidth;
     const cardWidth = $(".carousel-item").width();
     let scrollPosition = 0;
